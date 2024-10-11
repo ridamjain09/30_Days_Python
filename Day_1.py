fd9f802620cdb0a53d11a4afb21672e1b6bbf7a2
Ridam Jain <jainridam08@gmail.com>
2024-10-11 16:21:51 +0530

    Day_1

---
+++ Day_1.py
@@ -0,0 +1,23 @@
+print(10+3)    #Addition 
+print(10-5)    #Subtraction     
+print(10*2)    #Multiplication
+print(10/2)    #Division
+print(10**2)   #exponential
+
+
+#Checking types 
+print(type(10))
+print(type('ridam'))
+print(type([1,2,3]))
+print(type({'tom':'jerry'})) 
+
+
+
+
+
+a,b = 2,3
+c,d = 10,8
+
+p = ((a-c)**2 + (b-d)**2)
+
+print(p**0.5)
