f6f9da460407d612ccc3b87f7a3c386c6aa69795
Ridam Jain <jainridam08@gmail.com>
2024-10-18 20:32:14 +0530

    Day_5

--- Day_5.py
+++ Day_5.py
@@ -80,7 +80,109 @@ print(it_companies)
 print('Middle item: ',it_companies[len(it_companies)//2])
 print('First item ',it_companies[0])
 print('Last item',it_companies[-1])
-  
+five_item_list_1 = five_item_list.append('Realme')
+print(five_item_list_1)
 
 
+'''
+    Add an IT company to it_companies
+    Insert an IT company in the middle of the companies list
+    Change one of the it_companies names to uppercase (IBM excluded!)
+    Join the it_companies with a string '#;  '
+    Check if a certain company exists in the it_companies list.
+    Sort the list using sort() method
+    Reverse the list in descending order using reverse() method
+    Slice out the first 3 companies from the list
+    Slice out the last 3 companies from the list
+    Slice out the middle IT company or companies from the list
+    Remove the first IT company from the list
+    Remove the middle IT company or companies from the list
+    Remove the last IT company from the list
+    Remove all IT companies from the list
+    Destroy the IT companies list
+
+
+'''
+it_companies.append('Wipro')
+print(it_companies)
+middle_items = int(len(it_companies)//2)
+it_companies.insert(middle_items,'Mindtree')
+print(it_companies)
+it_companies.sort()
+print(it_companies)
+it_companies.reverse()
+print(it_companies)
+print(it_companies[:4])
+print(it_companies[4:])
+middle = len(it_companies)//2
+print(it_companies[middle])
+print(it_companies.pop(0))
+print(it_companies.pop(middle))
+print(it_companies.pop(-1))
+print(it_companies.clear())
+
+
+'''
+    Join the following lists:
+'''
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+
+
+joined_list = front_end + back_end
+print(joined_list)
+
+
+'''
+    After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack, 
+    then insert Python and SQL after Redux.
+
+'''  
+
+full_stack = joined_list.copy()
+print(full_stack)
+index_redux = full_stack.index('Redux')
+full_stack.insert(index_redux+1,'Python')
+full_stack.insert(index_redux+2,'SQL')
+print(full_stack)
+
+
+
+'''
+    Excersice level 2
+
+'''
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+
+'''
+    Sort the list and find the min and max age
+    Add the min age and the max age again to the list
+    Find the median age (one middle item or two middle items divided by two)
+    Find the average age (sum of all items divided by their number )
+    Find the range of the ages (max minus min)
+    Compare the value of (min - average) and (max - average), use abs() method
+
+'''
+
+ages.sort()
+print(ages)
+min_age = min(ages)
+max_age = max(ages)
+print(min_age)
+print(max_age)
+ages.extend([min_age,max_age])
+print(ages)
+median_of_age = ages[len(ages)//2]
+print(median_of_age)
+avg_age = sum(ages)//(len(ages))
+print(avg_age)
+range_of_ages = max_age - min_age
+print(range_of_ages)
+value_comparison_min = min_age-avg_age 
+value_comparison_max  = max_age-avg_age
+print(abs(value_comparison_max),'and', abs(value_comparison_min))
 
---
+++ test.py
@@ -0,0 +1,20 @@
+'''Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string ""
+'''
+
+'''
+
+Example 1: Inpput: strs = ["interspecies", "interstellar", "interstate"] Output: "inters"
+Example 2: Input strs = ["dog", "racecar", "car"] Output: "
+
+'''
+
+
+'''
+Reverse a string input = 'Rid@m.J@in'
+	Not changing the location of special characters
+
+    
+'''
+string_input = 'Rid@m.J@in'
+print(string_input[::-1])
